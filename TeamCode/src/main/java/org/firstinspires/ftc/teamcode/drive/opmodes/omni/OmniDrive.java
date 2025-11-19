df4d3886f24b53850d586217fbe558632f2c61c7
GhostKnight14 <ryan.sceusi29@paceacademy.org>
2025-11-19 11:03:09 -0500

    Ryan Pushing to Cole

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmodes/omni/OmniDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmodes/omni/OmniDrive.java
@@ -8,22 +8,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-
-@TeleOp(name="NIVEKdrive", group="drive")
 public class OmniDrive extends LinearOpMode {
 
 
 
     // Declare OpMode members for each of the 4 motors.
-    private ElapsedTime runtime = new ElapsedTime();
     private DcMotor frontLeftDrive = null;
     private DcMotor backLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor backRightDrive = null;
     private CRServo leftside = null;
     private CRServo rightside = null;
+
 
 
 
@@ -68,6 +67,7 @@ public class OmniDrive extends LinearOpMode {
 
 
         waitForStart();
+        ElapsedTime runtime = null;
         runtime.reset();
 
 
